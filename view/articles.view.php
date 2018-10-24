b7ab39cbee77ddbabac79c011f88e2431ee4f238
VACHER Camille <vacherca@hostname.iut2.upmf-grenoble.fr>
2018-10-24 16:42:17 +0200

    b

--- view/articles.view.php
+++ view/articles.view.php
@@ -11,47 +11,41 @@
   <header>
     <h1>La Moucherie</h1>
     <form class="formulaire" action="index.html" method="post">
-      <input class="champ" type="text" value="Search"/>
+      <input class="champ" type="text" placeholder="Recherche"/>
       <input class="bouton" type="button" value=" " />
     </form>
 
   </header>
 
-  <?php
-
-  ///////////////////////////////////////////////////////
-  //   A COMPLETER
-  //////////////////////////////////////////////////////
-  // Si une categorie est définie
-
-  ?>
-
-  <nav>
+  <nav id="menu">
     <ul>
-      <li><a href="afficherArticles.ctrl.php">Accueil</a></li>
-      <li><a href="afficherArticles.ctrl.php">Combat</a></li>
-      <li><a href="afficherArticles.ctrl.php">Compagnie</a></li>
-      <li><a href="afficherArticles.ctrl.php">Assistance</a></li>
-      <li><a href="afficherArticles.ctrl.php">Elevage</a></li>
+      <li><a href="afficherArticles.ctrl.php">ACCUEIL</a></li>
+      <li><a href="afficherArticles.ctrl.php">COMBAT</a>
+        <ul>
+            <li><a href="afficherArticles.ctrl.php">Soutiens</a></li>
+            <li><a href="afficherArticles.ctrl.php">Dégats</a></li>
+            <li><a href="afficherArticles.ctrl.php">Tank</a></li>
+        </ul>
+      </li>
+      <li><a href="afficherArticles.ctrl.php">COMPAGNIE</a></li>
+      <li><a href="afficherArticles.ctrl.php">ASSISTANCE</a></li>
+      <li><a href="afficherArticles.ctrl.php">ELEVAGE</a></li>
     </ul>
   </nav>
 
-  <?php
-  ///////////////////////////////////////////////////////
-  //  A COMPLETER
-  ///////////////////////////////////////////////
-  echo '<section>';
-  $PATH = 'http://www-info.iut2.upmf-grenoble.fr/intranet/enseignements/ProgWeb/data/bricomachin/img/';
-  foreach ($articles as $key => $article) {
-    $img = $PATH.$article->image;
-    echo '<article>';
-      echo "<p>$article->libelle</p>";
-      echo "<p><img src='$img' /></p>";
-      echo "<p>$article->prix</p>";
-    echo '</article>';
-  }
-  echo '</section>';
-  ?>
+    <?php
+    echo '<section>';
+    $PATH = 'http://www-info.iut2.upmf-grenoble.fr/intranet/enseignements/ProgWeb/data/bricomachin/img/';
+    foreach ($articles as $key => $article) {
+      $img = $PATH.$article->image;
+      echo '<article>';
+        echo "<p>$article->libelle</p>";
+        echo "<p><img src='$img' /></p>";
+        echo "<p>$article->prix</p>";
+      echo '</article>';
+    }
+    echo '</section>';
+    ?>
 
   <footer>
   </p>Site fictif, issus de données réelles du Web</p>
