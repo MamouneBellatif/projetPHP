2a2f280d8ab642db7e1530943d292a77994b1f19
BARET Dorian <baretd@hostname.iut2.upmf-grenoble.fr>
2018-11-01 00:09:27 +0100

    t

--- view/infoCompte.view.php
+++ view/infoCompte.view.php
@@ -1,1 +1,17 @@
- 
+<?php
+include("../view/header.php");
+
+  echo '<section>';
+    echo"<fieldset>";
+      echo"<legend> Vos information </legend>";
+        echo "<p>E-mail : $currentUser->mail</p>";
+        echo "<p></p>";
+        echo "<p>Nom : $currentUser->nom</p>";
+        echo "<p>Prenom : $currentUser->prenom</p>";
+        echo "<p></p>";
+        echo "<p>Type de compte : $currentUser->statut</p>";
+    echo"</fieldset>";
+  echo '</section>';
+
+include("../view/footer.php");
+?>
